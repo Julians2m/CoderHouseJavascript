8363f653843b96b96dcd62fccae1251848c51dad
Gustavo Mercado <gustavo.n.mercado2@gmail.com>
2023-04-11 14:25:45 -0300

    PrimeraEntregaMercado

---
+++ main.js/main.js
@@ -0,0 +1,44 @@
+function calcularCuotas() {
+    let monto = parseFloat(document.getElementById("monto").value);
+    let cuotas = parseInt(document.getElementById("cuotas").value);
+    let interes = parseFloat(document.getElementById("interes").value) / 100;
+
+    if (isNaN(monto) || isNaN(cuotas) || isNaN(interes)) {
+    document.getElementById("resultado").innerHTML =
+        "Error: ingresa valores numéricos.";
+    return;
+    }
+
+    let cuotaMensual = (monto * interes) / ((1 - (1 + interes) ** -cuotas));
+    let totalPagar = cuotaMensual * cuotas;
+    let totalInteres = totalPagar - monto;
+
+    document.getElementById(
+    "resultado"
+    ).innerHTML = `Monto a pagar por cuota mensual: $${cuotaMensual.toFixed(
+    2
+    )}.<br> Total a pagar: $${totalPagar.toFixed(2)}.<br> Interés total a pagar: $${totalInteres.toFixed(
+    2
+    )}.`;
+}
+
+function generarTablaAmortizacion() {
+    let monto = parseFloat(document.getElementById("monto").value);
+    let cuotas = parseInt(document.getElementById("cuotas").value);
+    let interes = parseFloat(document.getElementById("interes").value) / 100;
+
+    if (isNaN(monto) || isNaN(cuotas) || isNaN(interes)) {
+    document.getElementById("tabla-amortizacion").innerHTML =
+        "Error: ingresa valores numéricos.";
+    return;
+    }
+
+    let saldoPendiente = monto;
+    let cuotaMensual = (monto * interes) / (1 - ((1 + interes) ** (-cuotas)));
+
+}
+
+document.getElementById("calcular").addEventListener("click", calcularCuotas);
+document
+    .getElementById("generar-tabla")
+    .addEventListener("click", generarTablaAmortizacion);
